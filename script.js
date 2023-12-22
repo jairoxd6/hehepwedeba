615229d3667d73fabc92f625a1f7eec48a9a82f5
jairoxd6 <103599204+jairoxd6@users.noreply.github.com>
2023-12-23 00:28:17 +0800

    script.js

--- script.js
+++ script.js
@@ -4,12 +4,47 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+// Fireworks confetti code
+function fireworks() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    });
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    });
+  }, 250);
+}
+
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "sigiii";
   gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 
   // Hide the "No" button
   noBtn.style.display = "none";
+
+  // Trigger fireworks instead of confetti
+  fireworks();
 });
 
 noBtn.addEventListener("mouseover", () => {
